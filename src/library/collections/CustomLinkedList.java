17af9de10032428b7735433b335a12c930113863
EkaterinaSamsonova <katerina.koroban@gmail.com>
2024-09-07 21:58:54 -0700

    HW#5 1. Added 4 collections to the hierarchy. 2. Created a custom LinkedList with generic.

---
+++ src/library/collections/CustomLinkedList.java
@@ -0,0 +1,31 @@
+package library.collections;
+
+public class CustomLinkedList<T> {
+    private Node<T> head;
+
+    public CustomLinkedList() {
+        this.head = null;
+    }
+
+    public void append(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public void display() {
+        Node<T> current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
